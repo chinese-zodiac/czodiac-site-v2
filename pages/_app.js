157890137a8c03fe8b708d45a58bdeb358e77d8c
Plastic Digits <plasticdigits@protonmail.com>
2022-12-18 19:55:39 -0800

    Update description

--- pages/_app.js
+++ pages/_app.js
@@ -27,26 +27,26 @@ class MyApp extends App {
       <Container>
         <Head>
           <title>CZODIAC | The First Global Defi Bank</title>
-          <meta name="description" content= "Yield from assets backing CZUSD burn CZF with profits building a rising price floor." />
-          <meta name="robots" content= "index, follow"></meta>
-          <meta property="og:locale" content="en_EN"/>
-          
-        <meta property="og:title" content="CZODIAC | The First Global Defi Bank" />
-        <meta property="og:site_name" content="CZodiac" />
-        <meta property="og:url" content="https://czodiac.com" />
-        <meta property="og:description" content="Yield from assets backing CZUSD burn CZF with profits building a rising price floor." />
-        <meta property="og:type" content="article" />
-        <meta property="og:image" content={"https://czodiac.com"+OpenGraphImg} />
-        <meta property="og:image:width" content="1200" /> 
-        <meta property="og:image:height" content="630" />
+          <meta name="description" content="Yield from assets backing CZUSD burn CZR with profits building a rising price floor." />
+          <meta name="robots" content="index, follow"></meta>
+          <meta property="og:locale" content="en_EN" />
 
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:site" content="https://czodiac.com" />
-        <meta name="twitter:title" content="CZODIAC | The First Global Defi Bank"/>
-        <meta name="twitter:image" content={"https://czodiac.com"+OpenGraphImg} />
-        <meta name="twitter:image:width" content="1200"/>
-        <meta name="twitter:image:height" content="630"/>
-        <meta name="twitter:description" content="Yield from assets backing CZUSD burn CZF with profits building a rising price floor."/>
+          <meta property="og:title" content="CZODIAC | The First Global Defi Bank" />
+          <meta property="og:site_name" content="CZodiac" />
+          <meta property="og:url" content="https://czodiac.com" />
+          <meta property="og:description" content="Yield from assets backing CZUSD burn CZR with profits building a rising price floor." />
+          <meta property="og:type" content="article" />
+          <meta property="og:image" content={"https://czodiac.com" + OpenGraphImg} />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="https://czodiac.com" />
+          <meta name="twitter:title" content="CZODIAC | The First Global Defi Bank" />
+          <meta name="twitter:image" content={"https://czodiac.com" + OpenGraphImg} />
+          <meta name="twitter:image:width" content="1200" />
+          <meta name="twitter:image:height" content="630" />
+          <meta name="twitter:description" content="Yield from assets backing CZUSD burn CZR with profits building a rising price floor." />
 
 
         </Head>
